00fa6398fa096fc2b405e0f453ed2e670c47d346
Rotem Cohen <rotem@timeflash.com>
2015-08-31 21:55:43 +0300

    initial commit - Version 1.1 changeling from version 1.0: - icon.png changed from blue to grey - extension is activated on start-up (used to be toggled off on startup)

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # haaretzpass
 Switches User-Agent string to pass Haaretz payment wall
+
+This is a chrome extension I've made that disables the annoying payment wall on Haaretz website by changing the browser's user agent. I've made this last night after getting frustrated with all the other solutions for this that are available online.
+
+Instructions:
+A. Install crx file (see here for instructions on installing crx files)
+B. A green haaretz icon would appear in your browser toolbar.
+C. Go to haaretz website and enter a subscriptions-only article (marked by a grey key icon). If you already were in an Haaretz webpage when you activated the extension, you should refresh the page.
+D. Enjoy!
+
+You can leave the icon on even after leaving the haaretz website, it has permissions only for haaretz urls. Anyway, you can disable it anytime by clicking it.
+
+This code has no malware or any data collecting sh*t. 
+
+This has started as a modification of the chrome extenstion User-Agent Switcher (available on https://chrome.google.com/webstore/detail/user-agent-switcher/lkmofgnohbedopheiphabfhfjgkhfcgf?hl=en ) but almost none of its code remains.
---
+++ background.js
@@ -0,0 +1,49 @@
+var UA = null;
+var description = null;
+
+handler = function (details) {
+    
+    if (UA == null) {
+        return;
+    }
+
+    for (var i = 0, l = details.requestHeaders.length; i < l; ++i) {
+        if (details.requestHeaders[i].name === 'User-Agent') {
+            details.requestHeaders[i].value = UA;
+            break;
+        }
+    }
+
+    return { requestHeaders: details.requestHeaders };
+};
+
+if (UA != null) {
+    document.getElementById('ua').value = UA;
+}
+
+setUA('Googlebot/2.1 (+http://www.googlebot.com/bot.html)', 'Googlebot (Google\'s spider)');
+chrome.browserAction.setIcon({ path: ('icon-active.png') });    
+
+chrome.webRequest.onBeforeSendHeaders.addListener(handler, {urls: ["*://*.haaretz.co.il/*"]},  ["blocking", "requestHeaders"]);
+chrome.browserAction.onClicked.addListener(
+    function () {
+        if(UA == null) {
+            setUA('Googlebot/2.1 (+http://www.googlebot.com/bot.html)', 'Googlebot (Google\'s spider)');
+            return false;
+        } else {
+            clearUA();
+            return false;
+        }
+    });
+
+function setUA(ua, desc) {
+    UA = ua;
+    description = desc;
+    chrome.browserAction.setIcon({ path: (ua == null ? 'icon.png' : 'icon-active.png') });    
+}
+
+function clearUA() {
+    UA = null;
+    description = null;
+    chrome.browserAction.setIcon({ path: 'icon.png' }, function () { setTimeout('window.close()', 100); });    
+}
